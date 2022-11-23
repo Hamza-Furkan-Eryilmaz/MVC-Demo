cf6313d0bd23496cc0315c96de0810dbd1a33833
Hamza Furkan Eryılmaz <hf.eryilmaz@hotmail.com>
2022-11-24 00:54:49 +0300

    extract class

---
+++ Models/EditUserViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcDemo.Models
+{
+    public class EditUserViewModel
+    {
+
+        [Required(ErrorMessage = "Username is required.")]
+        [StringLength(30, ErrorMessage = "Username can be max 30 characters.")]
+        public string Username { get; set; }
+
+
+        [Required(ErrorMessage = "Full name is required.")]
+        [StringLength(30, ErrorMessage = "Full name can be max 30 characters.")]
+        public string FullName { get; set; }
+
+        public bool Locked { get; set; }       
+
+        [Required]
+        [StringLength(50)]
+        public string Role { get; set; } = "user";
+    }
+}
--- Models/UserViewModel.cs
+++ Models/UserViewModel.cs
@@ -54,23 +54,4 @@ namespace MvcDemo.Models
         [StringLength(50)]
         public string Role { get; set; }="user";
     }
-
-    public class EditUserViewModel
-    {
-
-        [Required(ErrorMessage = "Username is required.")]
-        [StringLength(30, ErrorMessage = "Username can be max 30 characters.")]
-        public string Username { get; set; }
-
-
-        [Required(ErrorMessage = "Full name is required.")]
-        [StringLength(30, ErrorMessage = "Full name can be max 30 characters.")]
-        public string FullName { get; set; }
-
-        public bool Locked { get; set; }       
-
-        [Required]
-        [StringLength(50)]
-        public string Role { get; set; } = "user";
-    }
 }
